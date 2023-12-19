b3a76e93d9817ca71157533d1477c8ed4200ca3c
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-19 23:37:23 +0100

    updated js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded',function(){
+const sidebar = document.querySelector('.sidebar');
+const hamburger = document.querySelector('.icon')
+const innerBurger= `<i class="bi bi-list"></i>`
+
+
+hamburger.addEventListener('click',()=>{
+   sidebar.classList.toggle("show")
+   if(sidebar.classList.contains("show")){
+      document.querySelector(".bi-list").style.backgroundColor = "white"  
+hamburger.innerHTML= innerBurger.replace("bi-list","bi-x")
+   }else{
+      hamburger.innerHTML = innerBurger
+   }
+    
+   
+})
+
+
 const questions = document.querySelectorAll('.question-wrapper')
 questions.forEach(question=>{
 question.addEventListener('click', function(){
